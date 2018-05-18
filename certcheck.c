4c50f63c62092d482235c4bdd8d935b966164f90
Nico Dinata <ncdnt@icloud.com>
2018-05-18 18:25:03 +1000

    Add method stub for validating certificate

--- certcheck.c
+++ certcheck.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <openssl/x509.h>
-// #include <openssl/x509v3.h>
-// #include <openssl/bio.h>
-// #include <openssl/pem.h>
-// #include <openssl/err.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/bio.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
 
 # define LINE_LENGTH 1024
 
+/* ---------------------- Helper function prototype ------------------------- */
+void validate_cert(FILE* output, char* path_to_cert, char* url);
+
+
+/* ----------------------------- Main Program ------------------------------- */
 int main(int argc, char const *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s path_to_csv_file\n", argv[0]);
@@ -36,14 +41,30 @@ int main(int argc, char const *argv[]) {
         char* path_to_cert = strtok(line, ",");
         char* url = strtok(NULL, ",\n");
 
-        // write content into output file
-        fprintf(output, "%s and %s\n", path_to_cert, url);
-    }
+        /* DEBUGGING -- TO BE REMOVED --------------------------------- */
+        // fprintf(output, "%s and %s\n", path_to_cert, url);
+        /* ------------------------------------------------------------ */
 
+        // handle certificate validation
+        validate_cert(output, path_to_cert, url);
+    }
     
     // close the input and output csv files
     fclose(output);
     fclose(fp);
 
     return EXIT_SUCCESS;
+}
+
+
+/* -------------------------------------------------------------------------- */
+/**
+ * Checks if the certificate located in `path_to_cert` is valid and writes the
+ * output to `output`.
+ * @param output file
+ * @param path_to_cert path to the certificate
+ * @param url purported domain name of the certificate
+ */
+void validate_cert(FILE* output, char* path_to_cert, char* url) {
+
 }
