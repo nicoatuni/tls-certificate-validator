3a6e66b2cbe8141b1209dd01bba9a27b2821cb63
Nico Dinata <ndinata@student.unimelb.edu.au>
2018-05-21 18:20:39 +1000

    Remove all debugging statements

--- certcheck.c
+++ certcheck.c
@@ -10,36 +10,24 @@
 #include <openssl/evp.h>
 #include <openssl/rsa.h>
 
-/* DEBUGGING -- REMOVE ------------------------------------------------- */
-#ifdef DEBUG
-#define DLOG(...) printf(__VA_ARGS__)
-#else
-#define DLOG(...) do {} while(0)
-#endif
-/* --------------------------------------------------------------------- */
-
-# define LINE_LENGTH 1024
-# define VALID 1
-# define INVALID 0
-# define CN_BUF_SIZE 1024
-# define BYTE_TO_BIT 8
-# define MIN_KEY_LENGTH 2048
+#define LINE_LENGTH 1024
+#define VALID 1
+#define INVALID 0
+#define CN_BUF_SIZE 1024
+#define BYTE_TO_BIT 8
+#define MIN_KEY_LENGTH 2048
 
 /* ---------------------- Helper function prototype ------------------------- */
 void validate_cert(FILE* output, char* path_to_cert, char* url);
-
-/* DEBUGGING -- REMOVE & UNCOMMENT BELOW ------------------------------- */
-int validate_dates(X509 *cert, char* path_to_cert);
-int validate_san(int cn_valid, X509* cert, char* url);
-/* --------------------------------------------------------------------- */
-// int validate_dates(X509 *cert);
-// int validate_san(X509* cert, char* url);
+int validate_dates(X509 *cert);
 int validate_domain(X509 *cert, char* url);
 int validate_cn(X509 *cert, char* url);
+int validate_san(X509* cert, char* url);
 int validate_name(char* buf, char* url);
 int validate_key_length(X509 *cert);
 int validate_basic_constraints(X509* cert);
 int validate_ext_key_usage(X509* cert);
+
 
 /* ----------------------------- Main Program ------------------------------- */
 int main(int argc, char const *argv[]) {
@@ -63,10 +51,6 @@ int main(int argc, char const *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    /* DEBUGGING -- REMOVE --------------------------------------------- */
-    DLOG("=========================================\n");
-    /* ----------------------------------------------------------------- */
-
     // read the contents of the input csv file
     char line[LINE_LENGTH];
     while (fgets(line, LINE_LENGTH, fp)) {
@@ -76,10 +60,6 @@ int main(int argc, char const *argv[]) {
         // handle certificate validation
         validate_cert(output, path_to_cert, url);
     }
-
-    /* DEBUGGING -- REMOVE --------------------------------------------- */
-    DLOG("=========================================\n");
-    /* ----------------------------------------------------------------- */
 
     // close the input and output csv files
     fclose(output);
@@ -123,10 +103,7 @@ void validate_cert(FILE* output, char* path_to_cert, char* url) {
 
     // cert contains the x509 certificate and is ready to be validated!
     // first, check the dates
-    /* DEBUGGING -- REMOVE & UNCOMMENT BELOW --------------------------- */
-    if (!validate_dates(cert, path_to_cert)) {
-    /* ----------------------------------------------------------------- */
-    // if (!validate_dates(cert)) {
+    if (!validate_dates(cert)) {
         fprintf(output, "%s,%s,%d\n", path_to_cert, url, INVALID);
         return;
     }
@@ -154,18 +131,6 @@ void validate_cert(FILE* output, char* path_to_cert, char* url) {
         fprintf(output, "%s,%s,%d\n", path_to_cert, url, INVALID);
         return;
     }
-    
-    // the minimum checking you are expected to do is as follows:
-    // 1. [X] validation of dates, both the `Not Before` and `Not After` dates
-    // 2. [X] domain name validation, including SAN extension, and wildcards
-    // 3. [X] minimum key length of 2048 bits for RSA
-    // 4. [ ] correct key usage, including extensions
-
-    // Part B Basic Certificate Checking (5 marks)
-    // – Reads input CSV and write output CSV (1 mark)
-    // – Correctly validates `Not Before` date (1 mark)
-    // – Correctly validates `Not After` date (1 mark)
-    // – Correctly validates domain name in Common Name (2 mark)
 
     // Part C Advanced Certificate Checking (5 marks)
     // – Correctly validates minimum RSA key length of 2048 bits (1 mark)
@@ -173,12 +138,6 @@ void validate_cert(FILE* output, char* path_to_cert, char* url) {
     //      ∗ BasicConstraints includes “CA:FALSE”
     //      ∗ Enhanced Key Usage includes “TLS Web Server Authentication”
     // – Correctly validates Subject Alternative Name extension (2 marks)
-
-    // A list of function calls you may NOT use is as follows:
-    // - X509_check_ca
-    // - X509_check_host
-    // - X509_cmp_current_time
-    // - X509_cmp_time
 
     // write output into the output file
     fprintf(output, "%s,%s,%d\n", path_to_cert, url, VALID);
@@ -193,10 +152,7 @@ void validate_cert(FILE* output, char* path_to_cert, char* url) {
  * @param cert  whose dates are to be validated
  * @return whether the dates are valid (1) or not (0)
  */
-/* DEBUGGING -- REMOVE & UNCOMMENT BELOW ------------------------------- */
-int validate_dates(X509 *cert, char* path_to_cert) {
-/* --------------------------------------------------------------------- */
-// int validate_dates(X509 *cert) {
+int validate_dates(X509 *cert) {
     int day, sec;
 
     // check the `notBefore` date
@@ -209,9 +165,6 @@ int validate_dates(X509 *cert, char* path_to_cert) {
     // if `day` or `sec` is positive, `notBefore` is in the future
     // hence, invalid cert
     if (day > 0 || sec > 0) {
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        DLOG("%-15s: `notBefore` is in the future (X)\n", path_to_cert);
-        /* ------------------------------------------------------------- */
         return INVALID;
     }
 
@@ -224,16 +177,10 @@ int validate_dates(X509 *cert, char* path_to_cert) {
 
     // if `day` or `sec` is negative, `notAfter` is in the past
     if (day < 0 || sec < 0) {
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        DLOG("%-15s: `notAfter` is in the past (X)\n", path_to_cert);
-        /* ------------------------------------------------------------- */
         return INVALID;
     }
 
     // dates are valid!
-    /* DEBUGGING -- REMOVE ----------------------------------------- */
-    DLOG("%-15s: Dates are valid (OK), ", path_to_cert);
-    /* ------------------------------------------------------------- */
     return VALID;
 }
 
@@ -249,7 +196,7 @@ int validate_domain(X509 *cert, char* url) {
     int cn_valid = validate_cn(cert, url);
 
     // check whether any of the Subject Alternative Names (SAN) corresponds to URL
-    int san_valid = validate_san(cn_valid, cert, url);
+    int san_valid = validate_san(cert, url);
 
     return (cn_valid || san_valid);
 }
@@ -270,10 +217,6 @@ int validate_cn(X509 *cert, char* url) {
         exit(EXIT_FAILURE);
     }
 
-    /* DEBUGGING -- REMOVE --------------------------------------------- */
-    DLOG("CN: ");
-    /* ----------------------------------------------------------------- */
-
     int is_valid = validate_name(cn_buf, url);
     free(cn_buf);
     return is_valid;
@@ -288,27 +231,11 @@ int validate_cn(X509 *cert, char* url) {
  * @param url       to which the cert's Subject Alternative Name is compared
  * @return whether they correspond (1) or not (0)
  */
-/* DEBUGGING -- REMOVE & UNCOMMENT BELOW ------------------------------- */
-int validate_san(int cn_valid, X509* cert, char* url) {
-/* --------------------------------------------------------------------- */
-// int validate_san(X509* cert, char* url) {
+int validate_san(X509* cert, char* url) {
     int loc = X509_get_ext_by_NID(cert, NID_subject_alt_name, -1);
     if (loc == -1) {
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        if (!cn_valid) {
-            DLOG("CN does not match URL + No SAN (X)\n");
-        }
-        /* ------------------------------------------------------------- */
         return INVALID;
     }
-
-    /* DEBUGGING -- REMOVE --------------------------------------------- */
-    if (cn_valid) {
-        DLOG("CN matches URL + SAN (OK), ");
-    } else {
-        DLOG("CN does not match URL + SAN (?), ");
-    }
-    /* ----------------------------------------------------------------- */
 
     // SAN(s) are present; obtain their value(s)
     X509_EXTENSION *ex = X509_get_ext(cert, loc);
@@ -337,9 +264,6 @@ int validate_san(int cn_valid, X509* cert, char* url) {
         char* flush = strtok_r(entry, ":", &end_san);
         char* san   = strtok_r(NULL, ":", &end_san);
 
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        DLOG("SAN: ");
-        /* ------------------------------------------------------------- */
         if (validate_name(san, url)) {
             return VALID;
         }
@@ -364,23 +288,15 @@ int validate_san(int cn_valid, X509* cert, char* url) {
 int validate_name(char* name, char* url) {
     // check if name matches URL outright
     if (!strncmp(url, name, strlen(url))) {
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        DLOG("Name matches URL (OK), ");
-        /* ------------------------------------------------------------- */
-
         return VALID;
     } else {
         // maybe it matches through a wildcard?
         if (name[0] == '*') {
             char* name_temp = name + 1;
             char* wildcard;
-
             wildcard = strstr(url, name_temp);
-            if (wildcard != NULL) {
-                /* DEBUGGING -- REMOVE --------------------------------- */
-                DLOG("Wilcard matches URL (OK), ");
-                /* ----------------------------------------------------- */
 
+            if (wildcard != NULL) {
                 // it _does_ match!
                 return VALID;
             }
@@ -412,15 +328,9 @@ int validate_key_length(X509 *cert) {
 
     // check if key length (in bits) is at least 2048 bits
     if ((key_length * BYTE_TO_BIT) >= MIN_KEY_LENGTH) {
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        DLOG("Key length >= 2048 bits (OK), ");
-        /* ------------------------------------------------------------- */
-
         return VALID;
     }
-    /* DEBUGGING -- REMOVE --------------------------------------------- */
-    DLOG("Key length < 2048 bits (X)\n");
-    /* ----------------------------------------------------------------- */
+
     return INVALID;
 }
 
@@ -433,9 +343,6 @@ int validate_key_length(X509 *cert) {
 int validate_basic_constraints(X509* cert) {
     int loc = X509_get_ext_by_NID(cert, NID_basic_constraints, -1);
     if (loc == -1) {
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        DLOG("%-15s  %-30s (0)\n", "", "Basic Constraints extension not present");
-        /* ------------------------------------------------------------- */
         return INVALID;
     }
 
@@ -471,18 +378,12 @@ int validate_basic_constraints(X509* cert) {
         char* is_ca = strtok_r(NULL, ":", &end_is_ca);
 
         if (!strncmp(is_ca, "FALSE", strlen("FALSE"))) {
-            /* DEBUGGING -- REMOVE ----------------------------------------- */
-            DLOG("CA: False (OK), ");
-            /* ------------------------------------------------------------- */
             return VALID;
         }
 
         entry = strtok_r(NULL, ",", &end_entry);
     }
 
-    /* DEBUGGING -- REMOVE ----------------------------------------- */
-    DLOG("CA: True (X)\n");
-    /* ------------------------------------------------------------- */
     return INVALID;
 }
 
@@ -493,9 +394,6 @@ int validate_basic_constraints(X509* cert) {
 int validate_ext_key_usage(X509* cert) {
     int loc = X509_get_ext_by_NID(cert, NID_ext_key_usage, -1);
     if (loc == -1) {
-        /* DEBUGGING -- REMOVE ----------------------------------------- */
-        DLOG("%-15s  %-30s (0)\n", "", "Extended Key Usage extension not present");
-        /* ------------------------------------------------------------- */
         return INVALID;
     }
 
@@ -521,17 +419,11 @@ int validate_ext_key_usage(X509* cert) {
 
     while (usage != NULL) {
         if (!strncmp(usage, LN_server_auth, strlen(LN_server_auth))) {
-            /* DEBUGGING -- REMOVE ----------------------------------------- */
-            DLOG("Server Auth (OK)\n");
-            /* ------------------------------------------------------------- */
             return VALID;
         }
 
         usage = strtok(NULL, ",");
     }
 
-    /* DEBUGGING -- REMOVE ----------------------------------------- */
-    DLOG("Non-Server Auth (X)\n");
-    /* ------------------------------------------------------------- */
     return INVALID;
 }
